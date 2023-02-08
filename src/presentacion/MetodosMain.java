7852c71f5225e8d6da5e0b864c204132a759bfd1
gastonferron <bruceferr@gmail.com>
2023-02-08 00:13:31 -0300

    AltaProveedor
    -Metodo altaProveedor creado en MetodosMain
    -Correccion de altaTurista en nombre del menu
    -Correccion de altaTurista mensaje de error
    -Cambio de posicion de ingresar nacionalidad

--- src/presentacion/MetodosMain.java
+++ src/presentacion/MetodosMain.java
@@ -42,13 +42,14 @@ public class MetodosMain {
 
         System.out.println("Ingrese que tipo de usuario quiere dar de alta");
         System.out.println("Opcion 1: Alta de Turista");
-        System.out.println("Opcion 2: ALta de Proveedor");
+        System.out.println("Opcion 2: Alta de Proveedor");
         int opcion = entrada.nextInt();
         switch (opcion){
             case 1:
                 altaTurista();
                 break;
             case 2:
+                altaProveedor();
                 break;
             default:
                 System.out.println("Ingrese una opcion valida");
@@ -73,6 +74,9 @@ public class MetodosMain {
         System.out.println("Ingrese email");
         String email = entrada.nextLine();
 
+        System.out.println("Ingrese su nacionalidad");
+        String nacionalidad = entrada.nextLine();
+
         System.out.println("Ingrese su fecha de nacimiento");
 
         System.out.println("Ingrese el año");
@@ -86,17 +90,52 @@ public class MetodosMain {
 
         LocalDate fechanac = LocalDate.of(anio , mes , dia);
 
-        System.out.println("Ingrese su nacionalidad");
-        String nacionalidad = entrada.nextLine();
-
         try{
             iCU.altaTurista(nickname, nombre, apellido, email, LocalDate.parse(fechanac.toString()), nacionalidad);
         }catch (EntidadExiste e){
-            System.out.println("El nickname del usuario ya esta en uso");
+            System.out.println("Ese usuario ya esta ingresado");
         }
     }
     public void altaProveedor(){
+        Scanner entrada = new Scanner(System.in);
+        Fabrica fabrica = Fabrica.getInstanciaFabrica();
+        IControladorUsuario iCU = fabrica.getIControladorUsuario();
 
+        System.out.println("Ingrese el nombre de usuario (nickname) del Proveedor");
+        String nickname = entrada.nextLine();
+
+        System.out.println("Ingrese el nombre");
+        String nombre = entrada.nextLine();
+
+        System.out.println("Ingrese el apellido");
+        String apellido = entrada.nextLine();
+
+        System.out.println("Ingrese email");
+        String email = entrada.nextLine();
+
+        System.out.println("Ingrese una descripcion");
+        String desc = entrada.nextLine();
+
+        System.out.println("Ingrese la url");
+        String url = entrada.nextLine();
+
+        System.out.println("Ingrese su fecha de nacimiento");
+
+        System.out.println("Ingrese el año");
+        int anio = entrada.nextInt();
+
+        System.out.println("Ingrese el mes");
+        int mes = entrada.nextInt();
+
+        System.out.println("Ingrese el dia");
+        int dia = entrada.nextInt();
+
+        LocalDate fechanac = LocalDate.of(anio , mes , dia);
+
+        try {
+            iCU.altaProveedor(nickname, nombre, apellido, email, desc, url, fechanac);
+        }catch (EntidadExiste e){
+            System.out.println("Ese Proveedor ya existe");
+        }
     }
-
 }
